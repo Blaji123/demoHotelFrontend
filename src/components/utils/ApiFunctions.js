74dc52baccf466a037aa0c853b0c8245f30ef69a
Blajan David <david_blajan523@yahoo.com>
2024-04-17 19:44:19 +0300

    initial commit

--- src/components/utils/ApiFunctions.js
+++ src/components/utils/ApiFunctions.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:9192",
+  baseURL: "demohotel-backend.azurewebsites.net",
 });
 
 export const getHeader = () => {
